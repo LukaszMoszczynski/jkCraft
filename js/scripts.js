d0410d1e2cb08394846f24ece7d646b7ffa13931
Łukasz Moszczyński <moszczynski.luk@gmail.com>
2018-11-22 20:13:29 +0100

    add map

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-$( document ).ready(function() {
+/*$( document ).ready(function() {
 
 });
+
+*/
+
+function initMap() {
+  // The location of Uluru
+  var jk = {lat: 53.4289036, lng: 14.5448369};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 16, center: jk});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: jk, map: map});
+}
